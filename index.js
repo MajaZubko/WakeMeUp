86875d9a67c9fe7a2a61293f43efb595f723e895
Maja Zubko <majamzubko@gmail.com>
2020-07-13 12:36:02 +0200

    added the sign in overlay, created usersRepo

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const execSync = require('child_process').execSync;
 const layout = require('./layout');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+	res.send(
+		layout({
+			content: `
+			<div id="addLinkContainer" class="full-screen-opaque">
+			<div class="panel" id="addLinkPanel">
+				<h3 class="header">Enter a password</h3>
+				<form id="authForm" method="POST">
+					<input type="password" name="password" id="password" placeholder="Password">
+
+					<div id="addedCategories">
+
+					</div>
+
+					<input type="submit" id="submitButton" value="Submit"> 	
+
+				</form>
+			</div>
+		</div>
+
+				<form>
+					<button>Wake me up</button>
+				</form>
+			`
+		})
+	);
+});
+
+app.post('/', async (req, res) => {
+	const { password } = req.body;
+});
+
+app.get('/signedin', (req, res) => {
 	res.send(
 		layout({
 			content: `
@@ -18,7 +51,7 @@ app.get('/', (req, res) => {
 	);
 });
 
-app.post('/', (req, res) => {
+app.post('/signedin', (req, res) => {
 	const output = execSync('dir', { encoding: 'utf-8' });
 	console.log('Output was:\n', output);
 	res.send(
